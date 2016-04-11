19c9fdaf69a87d0939f2efdfbfaaafad3c66f03c
Matthew Pill <matthew.pill88@gmail.com>
2016-04-11 20:35:52 +0100

    initial commit

---
+++ gallery.php
@@ -0,0 +1,14 @@
+<?php 
+$pageTitle = "SP Hair | Contact";
+include('includes/header.php'); ?>
+
+<div class="row">
+	<div class="contact_form large-centered columns">
+		<p class="subtitle fancy"><span>GALLERY</span></p>
+
+		
+
+	</div>
+</div>
+
+<?php include('includes/footer.php'); ?>
---
+++ testimonials.php
@@ -0,0 +1,33 @@
+<?php 
+$pageTitle = "SP Hair | Contact";
+include('includes/header.php'); ?>
+
+<div class="row">
+	<div class="contact_form large-centered columns">
+		<p class="subtitle fancy"><span>TESTIMONIALS</span></p>
+
+		<div class="row testimonial_copy">
+			<div class="large-12 columns">
+				<hr>
+				<h3>"Testimonial One Example..."</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat, ipsum vitae consequat egestas, dolor erat aliquet metus, nec euismod est neque non tellus./p>
+				<hr>
+			</div>
+			<div class="large-12 columns">
+				<hr>
+				<h3>"Testimonial Two Example..."</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat, ipsum vitae consequat egestas, dolor erat aliquet metus, nec euismod est neque non tellus./p>
+				<hr>
+			</div>
+			<div class="large-12 columns">
+				<hr>
+				<h3>"Testimonial Three Example..."</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat, ipsum vitae consequat egestas, dolor erat aliquet metus, nec euismod est neque non tellus./p>
+				<hr>
+			</div>
+		</div>
+		
+	</div>
+</div>
+
+<?php include('includes/footer.php'); ?>
